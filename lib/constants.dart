5f07a8c18446d666b902743ed139a3d7d151a507
Sung Pham Dinh <dinhsung92.uit@gmail.com>
2020-08-18 22:22:41 +0700

    add sandbox and constants

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kDefaultPadding = 20;
+const kDefaultMargin = 20;
+
+const kDefaultSmallFontSize = 12;
+const kDefaultMediumFontSize = 14;
+const kDefaultLargeFontSize = 18;
+
+const kDefaultTextColor = Colors.white;
+const kPrimaryColor = Colors.pink;
